1712de2fcb4fde958d72aa86375d6ec212403b57
giulio <66009328+lsd-cat@users.noreply.github.com>
2026-02-26 21:07:57 +0100

    feat: make build ids static

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   outputFileTracingRoot: require('path').join(__dirname),
+  generateBuildId: async () => {
+    return "build-repro";
+  }
 }
 
 module.exports = nextConfig
