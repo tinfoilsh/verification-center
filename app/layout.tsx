68366d4937d90a097fcf75accc2abb722fc5a8a1
Sacha Servan-Schreiber <8027773+sachaservan@users.noreply.github.com>
2026-08-15 02:01:33 -0400

    perf: render verification content without staged reveals

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,6 +13,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="preload" href="/fonts/aeonik-regular.woff2" as="font" type="font/woff2" crossOrigin="anonymous" />
+        <link rel="preload" href="/fonts/aeonikfono-regular.woff2" as="font" type="font/woff2" crossOrigin="anonymous" />
+        <link rel="preload" href="/icons/chevron-band-divider.svg" as="image" type="image/svg+xml" />
+        <link rel="preload" href="/icons/logo-green.svg" as="image" type="image/svg+xml" />
+        <link rel="preload" href="/icons/logo-white.svg" as="image" type="image/svg+xml" />
+      </head>
       <body>{children}</body>
     </html>
   )
